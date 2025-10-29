6ee8f7230ba023fe939b898ef546c95d2eac1c1f
neki-or <somaschini.marco2@gmail.com>
2025-10-29 22:23:03 +0100

    feat: define boot partition

--- hosts/senkai-nixos/disko-config.nix
+++ hosts/senkai-nixos/disko-config.nix
@@ -8,11 +8,8 @@
           type = "gpt";
           partitions = {
             boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-            };
-            ESP = {
-              size = "1G";
+              label = "BOOT";
+              size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
