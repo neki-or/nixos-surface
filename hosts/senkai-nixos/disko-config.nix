b5f1c5386060ba393ab21b319a5a0b074643935f
neki-or <somaschini.marco2@gmail.com>
2025-11-01 18:38:53 +0100

    feat: add "defaults" mount option for boot

--- hosts/senkai-nixos/disko-config.nix
+++ hosts/senkai-nixos/disko-config.nix
@@ -15,7 +15,7 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
+                mountOptions = [ "defaults" "umask=0077" ];
               };
             };
 
@@ -52,7 +52,6 @@
                 discardPolicy = "both";
               };
             };
-
           };
         };
       };
