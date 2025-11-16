b579f18d714a63c383ebe7f6ee9a0309a2420762
neki-or <somaschini.marco2@gmail.com>
2025-11-16 17:23:53 +0100

    feat(bluetooth): add systemd service for bluez

--- nixos/services.nix
+++ nixos/services.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   services = {
     # Audio
@@ -10,4 +11,46 @@
     libinput.enable = true;
 
   };
+
+  # Copied from my desktop with "Fedora" and KDE
+  systemd.services.bluetooth = let
+    bluez = pkgs.bluez.outPath;
+
+  in {
+    enable = true;
+    description = "Bluetooth service [defined in nix-surface flake]";
+
+    serviceConfig = {
+      Type = "dbus";
+      BusName = "org.bluez";
+      ExecStart = "${bluez}/lib/libexec/bluetooth/bluetoothd";
+      NotifyAccess = "main";
+      CapabilityBoundingSet = [ "CAP_NET_ADMIN" "CAP_NET_BIND_SERVICE" ];
+      LimitNPROC = 1;
+
+      # Filesystem lockdown
+      ProtectHome = true;
+      ProtectSystem = "strict";
+      PrivateTmp = true;
+      ProtectKernelTunables = true;
+      ProtectControlGroups = true;
+      StateDirectory = "bluetooth";
+      StateDirectoryMode = "0700";
+      ConfigurationDirectory = "bluetooth";
+      ConfigurationDirectoryMode = "0555";
+
+      # Execute Mappings
+      MemoryDenyWriteExecute = true;
+
+      # Privilege escalation
+      NoNewPrivileges = true;
+
+      # Real-time
+      RestrictRealtime = true;
+    };
+
+    wantedBy = [ "bluetooth.target" ];
+    aliases = [ "dbus-org.bluez.service" ];
+  };
+
 }
